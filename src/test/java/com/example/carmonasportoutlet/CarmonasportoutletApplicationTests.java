7828cf79d8a9230828b641402877c77f36656737
juaki <jcasquetsegura@safareyes.es>
2025-03-18 12:41:52 +0100

    arrancamos

---
+++ src/test/java/com/example/carmonasportoutlet/CarmonasportoutletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carmonasportoutlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmonasportoutletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
