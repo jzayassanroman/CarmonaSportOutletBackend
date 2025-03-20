96476cbd1f291b207c127c88506043c3b8091fbd
USUARIO <ppadillaarroyo@safareyes.es>
2025-03-20 21:41:27 +0100

    cruds principales

--- src/main/java/com/example/carmonasportoutlet/CarmonasportoutletApplication.java
+++ src/main/java/com/example/carmonasportoutlet/CarmonasportoutletApplication.java
@@ -2,8 +2,11 @@ package com.example.carmonasportoutlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.carmonasportoutlet.Controller", "com.example.carmonasportoutlet.Services"})
+
 public class CarmonasportoutletApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/carmonasportoutlet/Controller/ProductoController.java
+++ src/main/java/com/example/carmonasportoutlet/Controller/ProductoController.java
@@ -18,14 +18,17 @@ public class ProductoController {
     @Autowired
     private ProductoService productoService;
 
-    // Endpoint para crear un nuevo producto
+    public ProductoController() {
+        System.out.println("ProductoController cargado");
+    }
+
     @PostMapping("/crear")
     public ResponseEntity<Producto> crearProducto(@RequestBody Producto productos) {
         try {
-            Producto producto = productoService.crearProducto(productos);  // Llamamos al servicio para crear el producto
-            return new ResponseEntity<>(producto, HttpStatus.CREATED);  // Respondemos con el producto creado
+            Producto producto = productoService.crearProducto(productos);
+            return new ResponseEntity<>(producto, HttpStatus.CREATED);
         } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);  // Si el cliente no se encuentra, respondemos con un 404
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 }
